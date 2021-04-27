60eb3aec0abc99c235add274b655e47404fa3221
Thomas Sankara <thomassankara74@gmail.com>
2021-04-27 22:58:21 +0300

    testimonials data array

---
+++ data/testimonials.js
@@ -0,0 +1,32 @@
+// Data for "Testimonials" section
+// You can install uuid to generate unique ids for your project
+
+export const testimonials = [
+  {
+    id: 1,
+    name: "John Doe",
+    avatar: "./images/john-doe-1.jpg",
+    title: "CEO",
+    // For the quotes image - use an icon library or create your own svg
+    desc:
+      "He is a great tutor, always explains everything as best he can. His Udemy course has helped me get started in web development and I would recommend him 10 times out of 10.",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    avatar: "./images/john-doe-2.jpg",
+    title: "CEO",
+    // For the quotes image - use an icon library or create your own svg
+    desc:
+      "I've got more confident watching Sankara's livestreams on YouTube because I got to understand that I don't need to know everything. All I have to do is start!",
+  },
+  {
+    id: 3,
+    name: "Jane Doe",
+    avatar: "./images/jane-doe.jpg",
+    title: "CEO",
+    // For the quotes image - use an icon library or create your own svg
+    desc:
+      "Aside from being punctual, he is one of the best web developers I have ever worked with. As if being prompt and open to ideas isn't enough, he always delivers the best experience.",
+  },
+]
