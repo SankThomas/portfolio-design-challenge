6609c17023c20eb367f6d81821439b92107771f0
Thomas Sankara <thomassankara74@gmail.com>
2021-04-27 22:57:23 +0300

    footer data array

---
+++ data/footer.js
@@ -0,0 +1,41 @@
+// Data for the footer
+// You can install uuid to generate unique ids for your project
+
+export const footer = [
+  {
+    id: 1,
+    title: "About Me",
+    links: ["Project Portfolio", "Bio"],
+  },
+  {
+    id: 2,
+    title: "Contacts",
+    links: ["Phone: +254 710 456 782", "Email: tsbsankara@gmail.com"],
+  },
+  {
+    id: 3,
+    title: "Courses",
+    links: ["HTML and CSS", "ReactJs", "NextJs"],
+  },
+  {
+    id: 4,
+    title: "Blog",
+    links: ["Latest Articles", "Archives"],
+  },
+  {
+    // This repeats but don't worry about it
+    id: 5,
+    title: "Socials",
+    links: ["Project Portfolio", "Bio"],
+  },
+  {
+    id: 6,
+    title: "Privacy Policy",
+    links: ["Read our Privacy Policy", "Terms and Conditions"],
+  },
+  {
+    id: 7,
+    title: "Socials",
+    links: ["Twitter", "Instagram", "YouTube", "GitHub"],
+  },
+]
