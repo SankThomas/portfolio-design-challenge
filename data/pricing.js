e94dde72fe72702fe25e6f80eed2bae6a87edc51
Thomas Sankara <thomassankara74@gmail.com>
2021-04-27 22:57:54 +0300

    pricing compoonent data array

---
+++ data/pricing.js
@@ -0,0 +1,47 @@
+// Data for "Pricing" section
+// You can install uuid to generate unique ids for your project
+// For the checkmarks, use an icon library or create your own svg
+
+export default pricing = [
+  {
+    id: 1,
+    title: "1 on 1 Mentorship",
+    price: "$100/hour",
+    items: [
+      "Web development",
+      "Personalized lessons",
+      "Anything web development",
+      "ReactJs web development",
+      "Free course giveaway",
+    ],
+    button: "I choose Mentorship",
+  },
+  {
+    id: 2,
+    title: "Consultation",
+    price: "$150/hour",
+    items: [
+      "Web development",
+      "Personalized lessons",
+      "Anything web development",
+      "ReactJs web development",
+      "Free course giveaway",
+      "5% discount",
+    ],
+    button: "I choose Consultation",
+  },
+  {
+    id: 3,
+    title: "Sponsorships",
+    price: "$300/hour",
+    items: [
+      "Web development",
+      "Personalized lessons",
+      "Anything web development",
+      "ReactJs web development",
+      "Free course giveaway",
+      "25% discount",
+    ],
+    button: "I choose Sponsorship",
+  },
+]
