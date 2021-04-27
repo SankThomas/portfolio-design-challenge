324744fb83fc827b93fab728edb7f2e389fc9929
Thomas Sankara <thomassankara74@gmail.com>
2021-04-27 22:57:31 +0300

    navbar data array

---
+++ data/navbar.js
@@ -0,0 +1,31 @@
+// Data for navbar links
+// You can install uuid to generate unique ids for your project
+
+export const navbar = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  // You can add some more links to create a dropdown menu - which was my original intention OR you can create a grid of services/items in the corresponding page when you click the links! - Or anything more creative than that.
+  {
+    id: 2,
+    title: "Web Design",
+    url: "/web-design",
+  },
+  {
+    id: 3,
+    title: "Courses",
+    url: "/courses",
+  },
+  {
+    id: 4,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 5,
+    title: "YouTube",
+    url: "https://youtube.com/tsbsankara",
+  },
+]
