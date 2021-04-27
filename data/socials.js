a20b52f21cab0c1742ec190e150fd3e9d769fe2a
Thomas Sankara <thomassankara74@gmail.com>
2021-04-27 22:58:10 +0300

    socials section data array

---
+++ data/socials.js
@@ -0,0 +1,54 @@
+// Data for "Announcing...my socials" section
+// You can install uuid to generate unique ids for your project
+// The text is a bit long - you can use conditional rendering to shorten it
+
+export const socials = [
+  {
+    id: 1,
+    image: "./images/twitter.jpg",
+    title: "Twitter",
+    desc:
+      "Follow me on the Bird App called Twitter @TSBSankara. I sometimes tweet about mundane things, or things that interest me, mostlly retweets and replies to tweets. Disclaimer: Very little interests me. P.S I need more followers. Please follow me and make me happy. Thank you - (place a heart icon here)",
+    link: "Follow me on Twitter",
+  },
+  {
+    id: 2,
+    image: "./images/instagram.jpg",
+    title: "Instagram",
+    desc:
+      "This is the app I usually forget about because creating or taking good images to post is hard. But now that I've learnt how to use Figma, you can be sure to see more activity on my profile so be sure to follow me on Instagram as well.",
+    link: "Follow me on Instagram",
+  },
+  {
+    id: 3,
+    image: "./images/gumroad.jpg",
+    title: "Gumroad",
+    desc:
+      "I write better than I speak - for those of you who think I'm eloquent in my videos, I'm really not. That's the magic of editing. Aside from that, I wrote an eBook - please buy it - and I am also working on another even more massive one - no spoilers. To get updates, follow me on Gumroad and you will receive an email for discounts and all new announcements! Thank you.",
+    link: "Follow me on Gumroad",
+  },
+  {
+    id: 4,
+    image: "./images/youtube.jpg",
+    title: "YouTube",
+    desc:
+      'I started my YouTube channel on November 6th, 2020 and it is progressing very well. I make tutorials about web development in ReactJs, NextJs and soon to begin GatsbyJs. I do occasionally do HTML and CSS and I love doing Frontend Mentor Challenes which are "Design to Code" challenges. Subscribe to keep me happy and motivated. Thank you',
+    link: "Subscribe to my YouTube channel",
+  },
+  {
+    id: 5,
+    image: "./images/twitch.jpg",
+    title: "Twitch",
+    desc:
+      "My Twitch channel is fairly younger than my YouTube channel and it hasn't got the traction yet. To help me out, follow me on Twitch and we will learn web development together! I call it a win-win situation! And if you like the content, please consider donation to show your support. Thank you and see you there!",
+    link: "Follow me on Twitch",
+  },
+  {
+    id: 6,
+    image: "./images/github.jpg",
+    title: "GitHub",
+    desc:
+      'This is the vault for all my applications and websites. Click the link below to see the terrible code I used to write four months ago and learn the corrections I\'ve made, and if you like what you see, then star it or fork it and when you do, the "Follow" button is close by!',
+    link: "Follow me on GitHub",
+  },
+]
