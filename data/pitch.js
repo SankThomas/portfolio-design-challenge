46101c8e045f88e6d1dbcb4c6594902fd7311ed9
Thomas Sankara <thomassankara74@gmail.com>
2021-04-27 22:57:40 +0300

    pitch section data array

---
+++ data/pitch.js
@@ -0,0 +1,54 @@
+// Data for "This is my Pitch" section
+// You can install uuid to generate unique ids for your project
+// The text is a bit long - you can use conditional rendering to shorten it
+
+export const pitch = [
+  {
+    id: 1,
+    image: "./images/web-development.jpg",
+    title: "Web Development",
+    desc:
+      "I am primarily a web developer. I am an artist of the web. You give me a blank canvas and I give you art that helps you grow your online presence and business, always ensuring you get the value for your money. Let's talk business about your business! Click the button below.",
+    link: "Let's talk - Get a quote",
+  },
+  {
+    id: 2,
+    image: "./images/udemy-course.jpg",
+    title: "Udemy Course",
+    desc:
+      "Are you looking to start out in web development but have no idea where to begin? I have the answer for you. Check out my Udemy course where we go through the building blocks of web development and top it off by building and deploying an awesome hotel website with form submissions! Get the course by clicking the link below.",
+    link: "Buy my course",
+  },
+  {
+    id: 3,
+    image: "./images/book.jpg",
+    title: "Buy my eBook",
+    desc:
+      "I had a really tough times starting out as a developer. I had the skills but none of the experience. So I wrote a book about how I overcame this - only 57 pages and $5.99 on Gumroad. It's an interesting read. Be sure to grab your copy by clicking the button below.",
+    link: "Buy my eBook",
+  },
+  {
+    id: 4,
+    image: "./images/twitch.jpg",
+    title: "Twitch",
+    desc:
+      "I do live coding sessions on Twitch, and occasionally play chess - I'm terrible at it. I fail most of the time at the coding as well but hey, it's okay to fail because you learn more. Okay, it's not fun failing at centering a <div>. Join my Twitch community and let's learn together!",
+    link: "Follow me on Twitch",
+  },
+  {
+    id: 5,
+    image: "./images/youtube.jpg",
+    title: "My YouTube Channel",
+    desc:
+      "Alongside Twitch, I also upload video tutorials on YouTube and do occasional livestreams as well. From the feedback I've got, they seem to really help a lot of people which has encouraged me to continue doing YouTube stuff. Please subscribe to my channel by clicking the link below.",
+    link: "Subscribe to my YouTube channel",
+  },
+  {
+    id: 6,
+    image: "./images/discord.jpg",
+    title: "Discord",
+    desc:
+      "Recently I've started receiving more and more requests to help people and it was a bit hard for me to keep track of everyone who DMs me because of the multiple apps. So I created a Discord server specifically for these requests and also to give updates about myself, my livestreams, courses and video releases. Click the button below to join the Discord server.",
+    link: "Join my Discord server",
+  },
+]
